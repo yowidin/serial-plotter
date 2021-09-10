f56f3ef9f44bfde5745e25bc02a180b5ae32fbd3
YoWiDiN <yowidin@gmail.com>
2021-09-10 13:55:54 +0200

    WIP: CI Fixes

--- .ci/build.py
+++ .ci/build.py
@@ -1,8 +1,9 @@
 import os
+from subprocess import run
 
 if not os.path.exists('_build'):
     os.makedirs('_build')
 
-os.chdir('_build')
-os.system('conan install .. -b missing')
-os.system('conan build ..')
+kwargs = {'cwd': '_build', 'check': True}
+run(['conan', 'install', '..', '-b', 'missing'], **kwargs)
+run(['conan', 'build', '..'], **kwargs)
