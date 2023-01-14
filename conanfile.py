d798526c4a8b0d8b353376d51095f1b431d6eeb5
YoWiDiN <yowidin@gmail.com>
2023-01-14 11:39:04 +0100

    Removed private conan server
    And fixed CI for Windows and Ubuntu

--- conanfile.py
+++ conanfile.py
@@ -8,15 +8,21 @@ class Recipe(ConanFile):
     description = 'Interactive tool for plotting data received via a serial port'
     settings = 'os', 'arch', 'compiler', 'build_type'
 
-    generators = 'cmake'
+    generators = 'cmake', 'CMakeToolchain', 'CMakeDeps'
 
     requires = [
-        'boost/1.75.0@conan-burrito/stable',
-        'SDL2/2.0.14@conan-burrito/stable',
-        'imgui/1.83@conan-burrito/stable',
-        'implot/0.11@conan-burrito/stable',
-        'glad/0.1.34@conan-burrito/stable'
+        'boost/1.75.0',
+        'sdl/2.0.14',
+        'imgui/1.83',
+        'implot/0.11',
+        'glad/0.1.34'
     ]
+
+    keep_imports = True
+
+    def imports(self):
+        self.copy('*.h', src='res/bindings/', dst='bindings')
+        self.copy('*.cpp', src='res/bindings/', dst='bindings')
 
     def build(self):
         cmake = CMake(self)
